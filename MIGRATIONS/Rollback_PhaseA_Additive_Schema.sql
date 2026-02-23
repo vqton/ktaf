709d460c027a84fd6606cb02213b09841fda8b17
User <user@example.com>
2026-02-23 08:50:47 +0700

    abc

--- MIGRATIONS/Rollback_PhaseA_Additive_Schema.sql
+++
@@ -1,5 +0,0 @@
--- Rollback Phase A Additive Schema (drop created tables)
-DROP TABLE IF EXISTS AccountingPrinciples_Accrual;
-DROP TABLE IF EXISTS AccountingPrinciples_Prepaid;
-DROP TABLE IF EXISTS AccountingPrinciples_Provision;
-DROP TABLE IF EXISTS AuditHash_Baseline;
